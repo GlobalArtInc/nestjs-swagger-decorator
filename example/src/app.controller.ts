5f0686472420cdf5a1d7b0760b791be2d3412f2e
Roman Khabarov <roman@globalart.dev>
2025-05-06 21:18:40 +0200

    fix

---
+++ example/src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { SwaggerDocumentation } from '../../src/decorators/index';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @SwaggerDocumentation({
+    endpointDescription: 'test',
+    endpointSummary: 'test',
+  })
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
